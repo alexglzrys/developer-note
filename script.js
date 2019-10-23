1689a937b3dc3932ed2dc5e32b9287faf428bd01
Alejandro Gonzalez Reyes <jsconestilo@gmail.com>
2019-10-23 18:53:14 -0500

    Propiedades Computadas - HTML desde Markdown

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ new Vue({
   el: '#notebook',
   data() {
     return {
-      content: 'Este es el contenido de una nota',
+      content: 'Escribe tu nota en **markdown**',
     }
   },
+  // Las propiedades computadas son nuevas propiedades que combinan cualquier cantidad de propiedades de nuestro modelo, con la finalidad de hacer cálculos o transformaciones de sus datos en tiempo real.
+
+  // Los calculos son almacenados en caché para su posterior uso, y solo se vuelven a calcular si alguna de las propiedades que interviene en el calculo cambia.
+
+  // Finalmente, una propiedad computada se puede usar como cualquier propiedad decladara en el objeto o función data. asi como usarse dentro de otras propiedades computadas
+  computed: {
+    notePreview() {
+      // La propiedad computada retorna el contenido del modelo content (Markdown) parseado a HTML
+      return marked(this.content)
+    }
+  }
 })
