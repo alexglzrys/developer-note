bf89bb5b23bbb5de9813d5bcc9999a64363380af
Alejandro Gonzalez Reyes <jsconestilo@gmail.com>
2019-10-23 20:36:40 -0500

    LifeCicle Hooks - cargar nota guardada

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ new Vue({
   data() {
     return {
       content: 'Escribe tu nota en **markdown**',
+
+      // inicializar el contenido directamente también es posible. Este mecanismo hace que no sea necesario el hook created y por consecuencia no se llame a nuestro observador en content, puesto que no estamos cambiando la data por una nueva...
+      // content: localStorage.getItem('content') || 'Escribe **markdown** ahora'
+    }
+  },
+  // Los métodos son funciones reutilizables dentro de nuestra aplicación de Vuejs. Los cuales pueden ser invocados por medio de acciones en la vista de nuestra aplicación, por otros métodos, u observadores.
+  methods: {
+    saveNote(val, oldVal) {
+      console.log('Cambios guardados exitosamente:', val, 'antes', oldVal)
+      localStorage.setItem('content', val)
+      // Podemos acceder a propiedades y metodos de la instancia de Vue a través de la palabra clave this
+      this.reportOperation('saving')
+    },
+    reportOperation(name) {
+      console.log('La operación', name, 'se completó exitosamente')
     }
   },
   // Las propiedades computadas son nuevas propiedades que combinan cualquier cantidad de propiedades de nuestro modelo, con la finalidad de hacer cálculos o transformaciones de sus datos en tiempo real.
@@ -14,19 +29,13 @@ new Vue({
   },
   // Los observadores ejecutan una funcion asociada a una propiedad cuando esta última cambia su valor. Es posible pasar un objeto para establecer opciones de ejecución y monitoreo de profundidad en los datos a observar, sin embargo, la mayoría del tiempo no hace falta especificar opciones adicionales.
   watch: {
-    content(newVal, oldVal) {
-      console.log('Nuevo valor:', newVal, 'Valor anterior:', oldVal)
-      // Guardamos la nota justo en el momento que se le realiza un determinado cambio de contenido.
-      localStorage.setItem('content', newVal)
-    }
-    /*
-    content: {
-      handler(newVal, oldVal) {
-        console.log('nueva nota:', newVal, 'nota anterior:', oldVal)
-      },
-      immediate: true,  // Ejecutar el observador tan pronto como se cree el componente (default false)
-      deep: false,  // Revisar recursivamete cambios en objetos anidados (default false)
-    }
-    */
+    // En este caso, cuando sucede un cambio en el modelo content, se invoca el método saveNote. De forma implicita se mandan los parametros newVal y oldVal
+    content: 'saveNote'
+  },
+  // Gancho del ciclo de vida de los componentes de Vue, este se llama justo cuando la instancia de vue esta lista, es decir, se haya creado. (sin embargo el componente aun no esta montado ni renderizado en el DOM) - Este mecanismo nos permite cargar la nota nuevamente cuando se refresca la aplicación
+  created() {
+    // Recuperamos el contenido almacenado en localStorage para mostrar la nota guardada en la aplicación, en caso contrario, mostramos una cadena por defecto.
+    // Esto ocasiona que nuestro observer se ejecute, dado que Vue en primer lugar carga la data declarada en el modelo, y este gancho hace que se modifique parte del modelo.
+    this.content = localStorage.getItem('content') || 'Tu puedes escribir en **markdown**'
   }
 })
