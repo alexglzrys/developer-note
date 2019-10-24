5c4b7f6657f3aa60683ac4059174b49709895150
Alejandro Gonzalez Reyes <jsconestilo@gmail.com>
2019-10-23 23:53:14 -0500

    Salvar notas - watch deep - v-if y css dinámico

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ new Vue({
   el: '#notebook',
   data() {
     return {
-      content: 'Escribe tu nota en **markdown**',
-      notes: [],
+      // Recuperamos las notas almacenadas en localstorage, pero convertimos el valor retornado a su tipo de datos nativo (array de objetos). En caso contrario retornamos un array vacio
+      notes: JSON.parse(localStorage.getItem('notes')) || [],
+      // recuperamos el id de la ultima nota seleccionada, en caso contrario retornamos null (trabajar con id es mas práctico que con el objeto de nota completo)
+      selectedId: localStorage.getItem('selected-id') || null,
     }
   },
   // Los métodos son funciones reutilizables dentro de nuestra aplicación de Vuejs. Los cuales pueden ser invocados por medio de acciones en la vista de nuestra aplicación, por otros métodos, u observadores.
   methods: {
-    saveNote(val, oldVal) {
-      console.log('Cambios guardados exitosamente:', val, 'antes', oldVal)
-      localStorage.setItem('content', val)
-      // Podemos acceder a propiedades y metodos de la instancia de Vue a través de la palabra clave this
-      this.reportOperation('saving')
-    },
-    reportOperation(name) {
-      console.log('La operación', name, 'se completó exitosamente')
+    saveNotes() {
+      // Solo podemos almacenar cadenas en localStorage, por tanto, convertimos nuestro arreglo de objetos en cadena
+      localStorage.setItem('notes', JSON.stringify(this.notes))
+      console.log('Notas guardadas', new Date())
     },
     addNote() {
       // Agregar una nota con contenido predeterminado
@@ -29,27 +27,38 @@ new Vue({
       }
       // Añadir la nueva nota a la lista de notas
       this.notes.push(note)
+    },
+    selectNote(note) {
+      // Establecer el id de la nota seleccionada desde la vista
+      this.selectedId = note.id
     }
   },
   // Las propiedades computadas son nuevas propiedades que combinan cualquier cantidad de propiedades de nuestro modelo, con la finalidad de hacer cálculos o transformaciones de sus datos en tiempo real.
   computed: {
     notePreview() {
-      // La propiedad computada retorna el contenido del modelo content (Markdown) parseado a HTML
-      return marked(this.content)
+      // La propiedad computada retorna el contenido parseado de la nota seleccionada en la vista, en caso contrario, retorna vacio
+      return this.selectedNote ? marked(this.selectedNote.content) : ''
     },
     addButtonTitle() {
       return this.notes.length ? this.notes.length + ' notas añadidas' : '0 notas añadidas'
+    },
+    selectedNote() {
+      // Retornamos la nota cuyo id sea identico al id de la nota seleccionada en la vista, en caso contrario retorno un objeto preconstruido con la data de mi modelo
+      return this.notes.find(note => note.id === this.selectedId) // || { content: this.content }
     }
   },
   // Los observadores ejecutan una funcion asociada a una propiedad cuando esta última cambia su valor. Es posible pasar un objeto para establecer opciones de ejecución y monitoreo de profundidad en los datos a observar, sin embargo, la mayoría del tiempo no hace falta especificar opciones adicionales.
   watch: {
-    // En este caso, cuando sucede un cambio en el modelo content, se invoca el método saveNote. De forma implicita se mandan los parametros newVal y oldVal
-    content: 'saveNote'
+    notes: {
+      handler: 'saveNotes',
+      // Necesitamos observar profundamente los cambios en las propiedades de cada objeto almacenado en el modelo de tipo arreglo notes
+
+      // Por defecto solo se observan cambios de asignacion de valores simples, así como de agregar-remover-ordenar elementos de un arreglo. Pero no asignación de valores en atributos de objetos simples y anidados, ni mucho menos cambios en los valores de los elementos de un array 
+      deep: true,
+    },
+    selectedId(newVal) {
+      // guardar el id de la ultima nota seleccionada. La intensión es que si se recarga la app, se debe mostrar tal y como se dejó con la misma nota seleccionada
+      localStorage.setItem('selected-id', newVal)
+    }
   },
-  // Gancho del ciclo de vida de los componentes de Vue, este se llama justo cuando la instancia de vue esta lista, es decir, se haya creado. (sin embargo el componente aun no esta montado ni renderizado en el DOM) - Este mecanismo nos permite cargar la nota nuevamente cuando se refresca la aplicación
-  created() {
-    // Recuperamos el contenido almacenado en localStorage para mostrar la nota guardada en la aplicación, en caso contrario, mostramos una cadena por defecto.
-    // Esto ocasiona que nuestro observer se ejecute, dado que Vue en primer lugar carga la data declarada en el modelo, y este gancho hace que se modifique parte del modelo.
-    this.content = localStorage.getItem('content') || 'Tu puedes escribir en **markdown**'
-  }
 })
